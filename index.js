1f48c2c288d51cdc6fd539c8861de28b4d5ef632
root <root@ip-172-31-43-214.ap-south-1.compute.internal>
2024-07-18 11:02:57 +0000

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const moment = require('moment');
 const db = require('./db');
 const {commodityMain} = require("./utils/mainCommodities");
 const {states} = require("./utils/commodities");
 const port = 8811
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    preflightContinue: true,
+    optionsSuccessStatus: 204
+};
+
+// Use the CORS middleware with the configured options
+app.use(cors(corsOptions));
+
+// Handle preflight requests
+app.options('*', cors(corsOptions));
 app.get('/', async (req, res) => {
     const { commodity, district, market, date,state } = req.query;
 if (!state){
@@ -25,7 +39,7 @@ const formattedDate = moment(date, 'DD MMM YYYY')
     console.log(formattedDate);
     try {
         const query = "SELECT p1.* FROM prices p1 JOIN (SELECT market, MAX(formatteddate) AS max_date FROM prices WHERE commodity = ? AND formatteddate <= ? AND district = ? GROUP BY market) p2 ON p1.market = p2.market AND p1.formatteddate = p2.max_date WHERE p1.commodity = ? AND p1.district = ? ORDER BY p1.formatteddate DESC";
-        const params = [commodity, district, formattedDate, commodity, district];
+        const params = [commodity, formattedDate,district, commodity, district];
 	    console.log(query,params);
         const [rows,fields] = await db.query(query, params);
         console.log(rows);
