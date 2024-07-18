51dd733c1c1b505fd76cd3e9c580cc3e0665eaeb
Vaibhav083 <vaibhav@asqi.in>
2024-07-18 14:43:58 +0530

    Changes for the api market

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const app = express();
 const moment = require('moment');
-const db = require('./db')
+const db = require('./db');
+const {commodityMain} = require("./utils/mainCommodities");
+const {states} = require("./utils/commodities");
 const port = 8811
 app.get('/', async (req, res) => {
-    const { commodity, district, market, date } = req.query;
-
-//    try {
-//        const [rows] = await db.query('SELECT * FROM prices WHERE commodity = ? AND district = ? AND market = ? AND date = ?', [commodity, district, market, date]);
-//        res.json(rows);
-//    } catch (error) {
-//        console.error('Error fetching data:', error);
- //       res.status(500).json({ error: 'Internal Server Error' });
-//    }
- if (!commodity || !district || !market || !date) {
+    const { commodity, district, market, date,state } = req.query;
+if (!state){
+    let state_arr = [];
+    res.status(200).json(states);
+}
+if (!commodity){
+    let commodity_arr = [];
+    return res.status(200).json(commodityMain);
+}
+ if (!commodity || !district  || !date) {
         return res.status(400).json({ error: 'Missing required query parameters' });
     }
 const formattedDate = moment(date, 'DD MMM YYYY')
     .subtract(1, 'days') // Subtract one day
     .set({ hour: 18, minute: 30, second: 0 }) // Set time to 18:30:00
-    .toISOString(); 
+    .toISOString();
     console.log(formattedDate);
     try {
-        const query = 'SELECT * FROM prices WHERE commodity = ? AND district = ? AND market = ? AND formatteddate <= ? ORDER BY formatteddate DESC limit 1';
-        const params = [commodity, district, market, formattedDate];
-	console.log(query,params);
+        const query = "SELECT p1.* FROM prices p1 JOIN (SELECT market, MAX(formatteddate) AS max_date FROM prices WHERE commodity = ? AND formatteddate <= ? AND district = ? GROUP BY market) p2 ON p1.market = p2.market AND p1.formatteddate = p2.max_date WHERE p1.commodity = ? AND p1.district = ? ORDER BY p1.formatteddate DESC";
+        const params = [commodity, district, formattedDate, commodity, district];
+	    console.log(query,params);
         const [rows,fields] = await db.query(query, params);
         console.log(rows);
         res.json(rows);
